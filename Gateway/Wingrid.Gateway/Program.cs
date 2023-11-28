377ed6b5d53fda75f7f10ab70078eb19474db68b
Doug Labbe <dougjlabbe@gmail.com>
2023-11-27 17:57:19 -0700

    Continue CD setup

--- Gateway/Wingrid.Gateway/Program.cs
+++ Gateway/Wingrid.Gateway/Program.cs
@@ -5,11 +5,18 @@ using Wingrid.Gateway.Extensions;
 
 var builder = WebApplication.CreateBuilder(args);
 builder.AddAppAuthentication();
-builder.Configuration.AddJsonFile("ocelot.json", optional: false, reloadOnChange: true);
+if(builder.Environment.ToString()?.ToLower().Equals("production") == true)
+{
+    builder.Configuration.AddJsonFile("ocelot.Production.json", optional: false, reloadOnChange: true);
+}
+else
+{
+    builder.Configuration.AddJsonFile("ocelot.json", optional: false, reloadOnChange: true);
+}
+
 builder.Services.AddOcelot(builder.Configuration);
 
 
 var app = builder.Build();
-app.MapGet("/", () => "Hello World!");
 app.UseOcelot().GetAwaiter().GetResult();
 app.Run();
