6f0170d6be4c6a449d6d48fee704ddf74549d80a
Doug Labbe <dougjlabbe@gmail.com>
2023-12-27 12:53:46 -0700

    hangfire jwt auth

--- Source/Web/ClientApp/src/components/NavBar.tsx
+++ Source/Web/ClientApp/src/components/NavBar.tsx
@@ -37,7 +37,7 @@ const NavBar = () => {
 										Create Fixture
 									</Navbar.Link>
 									<a
-										href="/hangfire"
+										href={`/hangfire?jwt_token=${loginResult.token}`}
 										className="block py-2 pr-4 pl-3 md:p-0 border-b border-gray-100 text-gray-700 hover:bg-gray-50 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white md:border-0 md:hover:bg-transparent md:dark:hover:bg-transparent md:dark:hover:text-white md:hover:text-green-600"
 									>
 										Jobs
