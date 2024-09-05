589360ba60a5aea2128df8955b3c43f033056324
Doug Labbe <dougjlabbe@gmail.com>
2024-09-05 07:24:38 -0700

    Add migration to remove entry id

---
+++ Source/Web/Migrations/20240905142323_EventsId.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Wingrid.Migrations
+{
+    /// <inheritdoc />
+    public partial class EventsId : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Id",
+                table: "Entries");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "Id",
+                table: "Entries",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0);
+        }
+    }
+}
