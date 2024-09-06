a618f3b121108dfbbdda5822d711f270e1c8108f
Doug Labbe <dougjlabbe@gmail.com>
2024-09-06 14:39:16 -0700

    Update user stats

--- Source/Web/Migrations/20240906213901_NewUserStats.Designer.cs
+++ Source/Web/Migrations/20240906213901_NewUserStats.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 using Wingrid.Data;
@@ -11,9 +12,11 @@ using Wingrid.Data;
 namespace Wingrid.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20240906213901_NewUserStats")]
+    partial class NewUserStats
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ Source/Web/Migrations/20240906213901_NewUserStats.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Wingrid.Migrations
+{
+    /// <inheritdoc />
+    public partial class NewUserStats : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "Entries",
+                table: "UserStatistics",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<int>(
+                name: "TotalTieBreakerError",
+                table: "UserStatistics",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<int>(
+                name: "Wins",
+                table: "UserStatistics",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Entries",
+                table: "UserStatistics");
+
+            migrationBuilder.DropColumn(
+                name: "TotalTieBreakerError",
+                table: "UserStatistics");
+
+            migrationBuilder.DropColumn(
+                name: "Wins",
+                table: "UserStatistics");
+        }
+    }
+}
